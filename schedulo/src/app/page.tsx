553e64deea20392beedf93d04ea4c9b91b0915aa
L22005691 <mikael.le-cam@etu.univ-amu.fr>
2024-03-27 14:25:17 +0100

    init commit

---
+++ schedulo/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+      <div>
+        <p className="font-semibold">
+          Hello Schedulo
+        </p>
+        <Button variant="link">Click Me</Button>
+      </div>
+
+  );
+}
