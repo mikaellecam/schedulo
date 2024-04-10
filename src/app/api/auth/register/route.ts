2562be2ce0eec670bc1544b696a866b9b67e6bb3
L22005691 <mikael.le-cam@etu.univ-amu.fr>
2024-04-10 22:06:23 +0200

    added registration route

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,30 @@
+import {NextResponse} from "next/server";
+import { hash} from "bcrypt";
+import {sql} from "@vercel/postgres";
+import {z} from "zod";
+
+const emailSchema = z.string().email();
+
+export async function POST(request: Request){
+    try{
+        const {email, password} = await request.json();
+        emailSchema.parse(email);
+
+        const hashedPassword = await hash(password, 10);
+
+        const verifyEmailExists = await sql`SELECT * FROM users WHERE email = ${email}`;
+        if(verifyEmailExists.rowCount > 0){
+            return NextResponse.json({status: 400, message: "email already exists"});
+        }
+
+        await sql`
+            INSERT INTO users (email, password) 
+            VALUES (${email}, ${hashedPassword})
+        `;
+
+    } catch(e){
+        console.log({e});
+        return NextResponse.json({status: 500, message: "error"});
+    }
+    return NextResponse.json({status: 201, message: "success"});
+}
