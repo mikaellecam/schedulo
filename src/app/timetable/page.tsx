b701066cb63c11b4c537d512feb11571274d584e
L22005691 <mikael.le-cam@etu.univ-amu.fr>
2024-04-10 22:05:12 +0200

    added timetable page

---
+++ src/app/timetable/page.tsx
@@ -0,0 +1,9 @@
+import Calendar from "@/components/Calendar";
+
+export default function DashboardPage(){
+    return (
+        <div className="">
+            <Calendar />
+        </div>
+    );
+}
