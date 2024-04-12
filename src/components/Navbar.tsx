818c2f01c805f9cdf862950c9bc1c40272a59c5a
L22005691 <mikael.le-cam@etu.univ-amu.fr>
2024-04-13 00:12:10 +0200

    refactored from a tag to Link component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,13 @@
 import LogoutButton from "@/components/LogoutButton";
 import UserProfile from "@/components/UserProfile";
+import Link from "next/link";
 
 
 export default function Navbar(){
     return (
         <nav className="flex justify-between items-center">
             <div className="mx-4 my-3">
-                <a href="/" className="title">Schedulo</a>
+                <Link href="/" className="title">Schedulo</Link>
             </div>
             <div className="flex mx-4 my-3">
                 <UserProfile />
