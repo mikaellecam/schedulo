e6648a11749e0975972ce3703e51caed708345da
L22005691 <mikael.le-cam@etu.univ-amu.fr>
2024-04-13 00:13:50 +0200

    added logic to add custom information in the session

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,6 +6,21 @@ import {z} from "zod";
 
 const emailSchema = z.string().email();
 
+const userSchema = z.object({
+    id: z.number(),
+    email: z.string().email(),
+    first_name: z.string().optional().nullable(),
+    last_name: z.string().optional().nullable(),
+    faculty: z.string().optional().nullable(),
+    department: z.string().optional().nullable(),
+    year: z.string().optional().nullable(), //L2 or L3 as the year
+    campus: z.string().optional().nullable(),
+    group: z.string().optional().nullable(),
+    group_english: z.string().optional().nullable(),
+    group_pppe: z.string().optional().nullable(),
+});
+
+
 const handler = NextAuth({
     session: {
         strategy: 'jwt',
@@ -27,7 +42,9 @@ const handler = NextAuth({
                     SELECT * FROM users WHERE email = ${credentials?.email}
                     `;
 
-                    const user = response.rows[0];
+                    const user =  response.rows[0];
+
+                    if(!user) return null;
 
                     const passwordCorrect = await compare(credentials?.password || '', user.password);
 
@@ -35,6 +52,7 @@ const handler = NextAuth({
                         return {
                             id: user.id,
                             email: user.email,
+                            ...user,
                         };
                     }
                     return null;
@@ -43,7 +61,26 @@ const handler = NextAuth({
                 }
             }
         })
-    ]
+    ],
+    callbacks: {
+        async jwt({token, user}){
+            if(user){
+                const parsedUser= userSchema.parse(user);
+                token.id = parsedUser.id;
+                token.email = parsedUser.email;
+                parsedUser.campus = "LUMINY";
+                Object.assign(token, parsedUser);
+            }
+            return token;
+        },
+        async session({session, token}){
+            if(token){
+                const parsedToken = userSchema.parse(token);
+                session.user = {...parsedToken};
+            }
+            return session;
+        }
+    },
 });
 
 export { handler as GET, handler as POST };
