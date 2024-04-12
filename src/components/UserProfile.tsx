595b5d5af1b4c037ef4809d6a599035bfc5ac707
L22005691 <mikael.le-cam@etu.univ-amu.fr>
2024-04-13 00:14:33 +0200

    created placeholder UserProfile component

---
+++ src/components/UserProfile.tsx
@@ -0,0 +1,8 @@
+export default function UserProfile() {
+    return (
+        <div className="flex items-center">
+            <img src="https://avatars.githubusercontent.com/u/6223174?v=4" alt="Profile Picture" className="rounded-full h-8 w-8" />
+            <span className="mx-2">John Doe</span>
+        </div>
+    );
+}
