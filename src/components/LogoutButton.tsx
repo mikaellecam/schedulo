756626cf70e372cb8aa5b6a8fcd97344ff90698b
L22005691 <mikael.le-cam@etu.univ-amu.fr>
2024-04-12 18:45:29 +0200

    fixed logout callback

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import {signOut} from "next-auth/react";
+import {Button} from "@/components/ui/button";
 
 export default function LogoutButton(){
     return (
-      <span onClick={() => {
-          signOut();
-      }}>
-          Logout
-      </span>
+        <form action={() => {
+            signOut({callbackUrl: "/auth/login"}).then(() => {console.log("Logged out")});
+        }}>
+            <Button type={"submit"}>Logout</Button>
+        </form>
     );
 }
