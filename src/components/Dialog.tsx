e26614d8a5d874046081ead7d04737fca4c77cc5
L22005691 <mikael.le-cam@etu.univ-amu.fr>
2024-04-15 23:26:28 +0200

    reformatted typing and methods

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -3,15 +3,14 @@
 import {useSearchParams} from "next/navigation";
 import {useEffect, useRef} from "react";
 import {Button} from "@/components/ui/button";
+import Image from "next/image";
 
 type Props = {
     title: string,
-    onClose: () => void,
-    onOk: () => void,
     children: React.ReactNode,
 }
 
-export default function Dialog({ title, onClose, onOk, children} : Props){
+export default function Dialog({ title, children} : Props){
     const searchParams = useSearchParams();
     const dialogRef = useRef<null | HTMLDialogElement>(null);
     const showDialog = searchParams.get('showDialog');
@@ -24,34 +23,23 @@ export default function Dialog({ title, onClose, onOk, children} : Props){
        }
     }, [showDialog]);
 
-    const closeDialog = () => {
-      dialogRef.current?.close();
-      onClose();
-    };
 
-    const clickOk = () => {
-        onOk();
-        closeDialog();
-    };
 
     return showDialog === 'y'
         ? (
             <dialog ref={dialogRef}
                     className="fixed top-50 left-50 -translate-x-50 -translate-y-50 z-10 rounded-xl border-gray-800 border-2 backdrop:bg-gray-800/50">
-                <div className="w-[650px] max-w-fullbg-gray-200 flex flex-col">
-                    <div className="flex flex-row justify-between items-center mb-4 pt-2 px-5 border-b-2 border-gray-800">
+                <div className="w-[750px] h-fit max-w-fullbg-gray-200 flex flex-col">
+                    <div className="flex flex-row justify-between items-center mb-4 px-5 border-b-2 border-gray-800">
                         <h1 className="text-2xl my-4">{title}</h1>
-                        {/*<Button onClick={closeDialog}>x</Button>*/}
+                        <Button variant="outline" onClick={() => {dialogRef.current?.close()}}>
+                            <Image src="/closeModalIcon.svg" alt="Closing icon for modal" width={30} height={30}/>
+                        </Button>
                     </div>
-                    <div className="px-5">
+                    <div className="flex flex-col h-full justify-end">
                         {children}
-                        <div className="flex flex-row justify-end mt-2">
-                            <Button onClick={clickOk}>Save Changes</Button>
-                            <Button variant="outline" onClick={closeDialog}>Cancel</Button>
-                        </div>
                     </div>
                 </div>
             </dialog>
         ) : null;
-
 }
