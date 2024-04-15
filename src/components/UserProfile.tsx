76f7412f5901ab15aa087c9df673c44264132d03
L22005691 <mikael.le-cam@etu.univ-amu.fr>
2024-04-15 23:33:57 +0200

    added session logic to show correct information

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,8 +1,17 @@
-export default function UserProfile() {
+import Image from "next/image";
+import {getServerSession} from "next-auth";
+import {redirect} from "next/navigation";
+
+export default async function UserProfile() {
+    const session = await getServerSession();
+    if(!session) redirect("/auth/login");
+
+    const name = session.user.name ? session.user.name : "John Doe";
+
     return (
         <div className="flex items-center">
-            <img src="https://avatars.githubusercontent.com/u/6223174?v=4" alt="Profile Picture" className="rounded-full h-8 w-8" />
-            <span className="mx-2">John Doe</span>
+            <Image src="https://avatars.githubusercontent.com/u/6223174?v=4" width={30} height={30} alt="Profile Picture" className="rounded-full h-8 w-8" />
+            <span className="mx-2">{name}</span>
         </div>
     );
 }
