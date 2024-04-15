48ea0d119e4d12b746eca476cbe243a0033a458f
L22005691 <mikael.le-cam@etu.univ-amu.fr>
2024-04-15 23:30:31 +0200

    session naming refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import {redirect} from "next/navigation";
 export default async function Home() {
   const session = await getServerSession();
   if(session) {
-    console.log(session.user.first_name);
-    if(session.user.first_name === undefined)
+    console.log(session.user.name);
+    if(session.user.name === undefined)
       redirect("/timetable?showDialog=y")
     else
       redirect("/timetable");
