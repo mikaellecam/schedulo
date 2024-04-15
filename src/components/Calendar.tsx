8ba0aa40ffae05840dc24f9a6253c45ad41681ca
L22005691 <mikael.le-cam@etu.univ-amu.fr>
2024-04-15 23:25:46 +0200

    calendar now containing fetched events

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -4,32 +4,58 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import iCalendarPlugin from "@fullcalendar/icalendar";
 import "../styles/timetable.css";
+import {useEffect, useState} from "react";
+import {Skeleton} from "@/components/ui/skeleton";
 
 export default function Calendar(){
+    const [events, setEvents] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect( () => {
+        fetch("/api/calendar")
+           .then(response => response.json())
+           .then(json => {
+                setEvents(json.data);
+                setLoading(false);
+           })
+           .catch(error => {
+                console.error("Error fetching events :", error);
+                setLoading(false);
+           });
+        }, []);
+
     return (
-        <div className="calendarContainer">
-            <FullCalendar
-                height={"100%"}
-                contentHeight={"auto"}
-                handleWindowResize={true}
+        <div className="my-0 mx-auto w-[80vw]">
+            {loading ? (
+                <Skeleton className="h-full w-full rounded-xl"/>
+            ) : (
+                <FullCalendar
+                    contentHeight={"auto"}
+                    handleWindowResize={true}
 
-                plugins = {[ timeGridPlugin, dayGridPlugin, interactionPlugin ]}
-                initialView = "timeGridWeek"
+                    plugins = {[
+                        timeGridPlugin,
+                        dayGridPlugin,
+                        interactionPlugin,
+                        iCalendarPlugin ]}
 
-                weekends={false}
-                allDaySlot={false}
+                    initialView = "timeGridWeek"
 
-                slotMinTime={"08:00:00"}
-                slotMaxTime={"20:00:00"}
+                    weekends={false}
+                    allDaySlot={false}
 
-                editable={true}
-                selectable={true}
+                    slotMinTime={"08:00:00"}
+                    slotMaxTime={"20:00:00"}
 
-                initialEvents={[
-                    { title: 'test event', start: '2024-04-08T10:00:00', end: '2024-04-08T12:00:00'}
-                ]}
-            />
+                    editable={true}
+                    selectable={true}
+
+                    eventTextColor={"black"}
+                    events={events}
+                />
+            )}
         </div>
     );
 }
