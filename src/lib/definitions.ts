a5d08cfdac84d8309ba0e1d597819f8af4d3ef0e
L22005691 <mikael.le-cam@etu.univ-amu.fr>
2024-04-17 00:26:40 +0200

    added new definitions for ease of use

---
+++ src/lib/definitions.ts
@@ -0,0 +1,19 @@
+import {z} from "zod";
+
+export const emailSchema = z.string().email();
+export const passwordSchema = z.string().min(6);
+
+export const userSchema = z.object({
+    id: z.string(),
+    email: z.string().email(),
+    name: z.string(),
+    groups: z.string(),
+});
+
+export const dataBaseUserSchema = z.object({
+    email: z.string().email(),
+    newPassword: z.string().min(6),
+    name: z.string(),
+    groups: z.string(),
+});
+
