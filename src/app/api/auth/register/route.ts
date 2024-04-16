83fbefe963286617d1af50b1ece3b67a682bfe6c
L22005691 <mikael.le-cam@etu.univ-amu.fr>
2024-04-17 00:30:11 +0200

    updating register to remove null values from app

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,29 +1,31 @@
 import {NextResponse} from "next/server";
-import { hash} from "bcrypt";
+import {hash} from "bcryptjs";
 import {sql} from "@vercel/postgres";
-import {z} from "zod";
-
-const emailSchema = z.string().email();
+import { emailSchema } from "@/lib/definitions";
 
 export async function POST(request: Request){
     try{
         const {email, password} = await request.json();
-        emailSchema.parse(email);
+        const parsedEmail = emailSchema.parse(email);
 
         if(!email || !password){
             return NextResponse.json({status: 400, message: "missing fields"});
         }
 
-        const hashedPassword = await hash(password, 10);
+        const verifyEmailExists = await sql`
+                                                                SELECT * 
+                                                                FROM users 
+                                                                WHERE email = ${parsedEmail}`;
 
-        const verifyEmailExists = await sql`SELECT * FROM users WHERE email = ${email}`;
         if(verifyEmailExists.rowCount > 0){
             return NextResponse.json({status: 400, message: "email already exists"});
         }
 
+        const hashedPassword = await hash(password, 10);
+
         await sql`
-            INSERT INTO users (email, password) 
-            VALUES (${email}, ${hashedPassword})
+            INSERT INTO users (email, password, name, groups) 
+            VALUES (${email}, ${hashedPassword}, '', '')
         `;
 
     } catch(e){
