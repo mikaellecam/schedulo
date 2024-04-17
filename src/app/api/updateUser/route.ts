7cb031cd8c1b7bdd5b506915e2a2c419297a79da
L22005691 <mikael.le-cam@etu.univ-amu.fr>
2024-04-17 02:10:27 +0200

    removed test logs

--- src/app/api/updateUser/route.ts
+++ src/app/api/updateUser/route.ts
@@ -4,7 +4,6 @@ import {dataBaseUserSchema, passwordSchema} from "@/lib/definitions";
 import {auth} from "@/auth";
 import {hash} from "bcryptjs";
 
-
 export async function POST(request: Request) {
     try {
         const session = await auth();
@@ -14,15 +13,10 @@ export async function POST(request: Request) {
 
         const {name, groups, email, newPassword} = dataBaseUserSchema.parse(json);
 
-        console.log("Request attributes : ", name, groups, email, newPassword);
-
         const verifyEmailExists = await sql`SELECT * FROM users WHERE email = ${email}`;
         if(verifyEmailExists.rowCount > 0 && verifyEmailExists.rows[0].email !== session.user.email){
-            console.log("pass");
             return NextResponse.json({status: 400, message: "email already exists"});
         }
-
-        console.log("Session: ", session);
 
         await sql`
                 UPDATE users
@@ -31,10 +25,8 @@ export async function POST(request: Request) {
             `;
 
 
-        let usingNewPassword = false;
         if (newPassword !== "" && passwordSchema.parse(newPassword)) {
             const hashedNewPassword = await hash(newPassword, 10);
-            usingNewPassword = true;
 
             await sql`
                 UPDATE users
@@ -42,11 +34,6 @@ export async function POST(request: Request) {
                 WHERE id = ${session.user.id}
             `;
         }
-        console.log("Session before updating: ", session.user);
-
-        //await unstable_update(session);
-
-        console.log("Session after updating: ", session.user);
 
         return NextResponse.json({status: 200, message: "success"});
     }catch (e) {
