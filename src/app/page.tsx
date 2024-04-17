98f59f21ae91a13079d0f61d221649e68f637d86
L22005691 <mikael.le-cam@etu.univ-amu.fr>
2024-04-17 02:10:46 +0200

    remove test log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import {redirect} from "next/navigation";
 export default async function Home() {
   const session = await auth();
   if(session) {
-    console.log(session.user.name);
     if(session.user.name === "")
       redirect("/timetable?showDialog=y")
     else
