bbbb38e23d2e7f184952e72e3b0817e22a935b48
L22005691 <mikael.le-cam@etu.univ-amu.fr>
2024-08-28 20:57:27 +0200

    Added SessionProvider for useSession hook

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { SessionProvider } from "next-auth/react";
 
 export const metadata: Metadata = {
   title: "Landing Page",
@@ -14,7 +15,9 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        <SessionProvider>
+          {children}
+        </SessionProvider>
       </body>
     </html>
   );
