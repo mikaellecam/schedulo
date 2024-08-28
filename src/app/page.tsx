5053a2f9a170671a0ac0fe40b8eaaf4b3558d8fa
L22005691 <mikael.le-cam@etu.univ-amu.fr>
2024-08-28 20:57:11 +0200

    Removed showDialog redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,6 @@ import {redirect} from "next/navigation";
 export default async function Home() {
   const session = await auth();
   if(session) {
-    if(session.user.name === "")
-      redirect("/timetable?showDialog=y")
-    else
       redirect("/timetable");
   } else
       redirect("/auth/login");
